d2e83cfd92da5728633ec5b6a5d83bddfe54e799
kyohei.uda <fcm1kyohei@gmail.com>
2020-08-09 10:31:25 +0000

    あああ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root to: 'tasks#index'
     
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
