f8c8e0297d8228bc9481e421798bbf512bb739f4
kyohei.uda <fcm1kyohei@gmail.com>
2020-08-10 01:24:13 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   
   resources :tasks,only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
